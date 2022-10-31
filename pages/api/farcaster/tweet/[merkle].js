c43ee19215baeaed49ff81469d5ac96b4ba5dd7d
Vincent Liao <vincent.liao@protonmail.com>
2022-10-31 14:12:34 +0700

    hardcode endpoint

--- pages/api/farcaster/tweet/[merkle].js
+++ pages/api/farcaster/tweet/[merkle].js
@@ -3,9 +3,11 @@ import { twitterClient } from './client';
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
+    const dev = process.env.NODE_ENV !== 'production';
+    const serverUrl = dev ? 'http://localhost:3000' : 'https://bot-monorepo.vercel.app/';
     const { merkle } = req.query;
 
-    const imageData = await fetch(`${process.env.VERCEL_URL}/api/farcaster/og?merkleRoot=${merkle}`);
+    const imageData = await fetch(`${serverUrl}/api/farcaster/og?merkleRoot=${merkle}`);
     const imageArrayBuffer = await imageData.arrayBuffer();
     const imageBuffer = Buffer.from(imageArrayBuffer);
     const mediaId = await twitterClient.v1.uploadMedia(imageBuffer, { mimeType: EUploadMimeType.Png });
