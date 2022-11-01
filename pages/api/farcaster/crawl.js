712ead5f7881a4543edb4a3e606b422141b22ad2
Vincent Liao <vincent.liao@protonmail.com>
2022-11-01 11:59:16 +0700

    log supabase instance

--- pages/api/farcaster/crawl.js
+++ pages/api/farcaster/crawl.js
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
+console.log(supabase)
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
